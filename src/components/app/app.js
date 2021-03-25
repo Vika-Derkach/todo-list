cb6f5befcc717741b2368266d39a1bc0a738a9b7
Vika <queenviktoriyphp@gmail.com>
2021-03-25 21:12:43 +0200

    доробили пошук

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,6 +16,8 @@ export default class App extends Component {
       this.createTodoItem("Make Awesome App"),
       this.createTodoItem("Have a lunch"),
     ],
+    term: "",
+
     // todoForShow: this.todoData,
   };
   createTodoItem(label) {
@@ -108,9 +110,21 @@ export default class App extends Component {
       };
     });
   };
-
+  //поиск з ним
+  search(items, term) {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+    });
+  }
+  onSearchChange = (term) => {
+    this.setState({ term });
+  };
   render() {
-    const { todoData } = this.state;
+    const { todoData, term } = this.state;
+    const visibleItems = this.search(todoData, term);
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
     console.log({ todoData });
@@ -118,7 +132,7 @@ export default class App extends Component {
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel onSearch={this.searchThings} />
+          <SearchPanel onSearchChange={this.onSearchChange} />
           <ItemStatusFilter
             onAllFilter={this.onAllFilter}
             onActiveFilter={this.onActiveFilter}
@@ -127,7 +141,7 @@ export default class App extends Component {
         </div>
 
         <TodoList
-          todos={todoData}
+          todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -2,36 +2,44 @@ import React, { Component } from "react";
 
 import "./search-panel.css";
 export default class SearchPanel extends Component {
-  state = {
-    label: "",
-  };
-  onSearchChange = (e) => {
-    this.setState({
-      label: e.target.value,
-    });
-  };
-  onSubmit = (e) => {
-    e.preventDefault();
-    this.props.onSearch(this.state.label);
-    // this.setState({
-    //   label: "",
-    // });
-  };
+  // state = {
+  //   label: "",
+  // };
+  // onSearchChange = (e) => {
+  //   this.setState({
+  //     label: e.target.value,
+  //   });
+  // };
+  // onSubmit = (e) => {
+  //   e.preventDefault();
+  //   this.props.onSearch(this.state.label);
+  //   // this.setState({
+  //   //   label: "",
+  //   // });
+  // };
   // onSearchChange = (e) => {
   //   console.log(e.target.value);
   // };
-
+  //його
+  state = {
+    term: "",
+  };
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  };
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          type="text"
-          className="form-control search-input"
-          placeholder="type to search"
-          onChange={this.onSearchChange}
-          // value={this.state.label}
-        />
-      </form>
+      // <form onSubmit={this.onSubmit}>
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="type to search"
+        onChange={this.onSearchChange}
+        value={this.state.term}
+      />
+      // </form>
     );
   }
 }
