5afec61bb9a73a6beabb12810a5658d39e5396bb
Vika <queenviktoriyphp@gmail.com>
2021-03-26 17:49:07 +0200

    добавила filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,8 +17,7 @@ export default class App extends Component {
       this.createTodoItem("Have a lunch"),
     ],
     term: "",
-
-    // todoForShow: this.todoData,
+    filter: "done", //active, all, done
   };
   createTodoItem(label) {
     return {
@@ -83,33 +82,7 @@ export default class App extends Component {
       };
     });
   };
-  onDoneFilter = (done) => {
-    this.setState(({ todoData }) => {
-      const newDoneArr = todoData.filter((el) => el.done === true);
 
-      return {
-        todoData: newDoneArr,
-      };
-    });
-  };
-  onActiveFilter = (done) => {
-    this.setState(({ todoData }) => {
-      const newActiveArr = todoData.filter((el) => el.done === false);
-
-      return {
-        todoData: newActiveArr,
-      };
-    });
-  };
-  onAllFilter = (done) => {
-    this.setState(({ todoData }) => {
-      const newAllArr = todoData.sort();
-
-      return {
-        todoData: newAllArr,
-      };
-    });
-  };
   //поиск з ним
   search(items, term) {
     if (term.length === 0) {
@@ -122,9 +95,26 @@ export default class App extends Component {
   onSearchChange = (term) => {
     this.setState({ term });
   };
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  };
+
+  filter(items, filter) {
+    switch (filter) {
+      case "all":
+        return items;
+      case "active":
+        return items.filter((item) => !item.done);
+      case "done":
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    }
+  }
   render() {
-    const { todoData, term } = this.state;
-    const visibleItems = this.search(todoData, term);
+    const { todoData, term, filter } = this.state;
+    const visibleItems = this.filter(this.search(todoData, term), filter);
+    // const visibleDoneItems = this.doneItem(todoData, term);
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
     console.log({ todoData });
@@ -134,9 +124,8 @@ export default class App extends Component {
         <div className="top-panel d-flex">
           <SearchPanel onSearchChange={this.onSearchChange} />
           <ItemStatusFilter
-            onAllFilter={this.onAllFilter}
-            onActiveFilter={this.onActiveFilter}
-            onDoneFilter={this.onDoneFilter}
+            filter={filter}
+            onFilterChange={this.onFilterChange}
           />
         </div>
 
--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -2,80 +2,29 @@ import React, { Component } from "react";
 
 import "./item-status-filter.css";
 export default class ItemStatusFilter extends Component {
-  state = {
-    done: false,
-    // show: false,
-  };
-  onDoneClick = (e) => {
-    e.preventDefault();
-    this.setState({
-      done: e.target.done,
-    });
-    this.props.onDoneFilter(this.state.done);
-  };
-
-  onActiveClick = (e) => {
-    e.preventDefault();
-    this.setState({
-      done: e.target.done,
-    });
-    this.props.onActiveFilter(this.state.done);
-  };
-  onAllClick = (e) => {
-    e.preventDefault();
-    this.setState({
-      done: e.target.done,
-    });
-    this.props.onAllFilter(this.state.done);
-  };
+  buttons = [
+    { name: "all", label: "All" },
+    { name: "active", label: "Active" },
+    { name: "done", label: "Done" },
+  ];
   render() {
-    // const { show } = this.props;
+    const { filter, onFilterChange } = this.props;
+    const buttons = this.buttons.map(({ name, label }) => {
+      const isActive = filter === name;
 
-    // let classNames = "btn btn-outline-secondary";
-    // if (show) {
-    //   classNames += " btn-info";
-    // }
-    return (
-      <div className="btn-group">
+      const clazz = isActive ? "btn-info" : "btn-outline-secondary";
+      return (
         <button
           type="button"
-          className="btn btn-outline-secondary btn-info"
-          onClick={this.onAllClick}
+          className={`btn ${clazz}`}
+          key={name}
+          onClick={() => onFilterChange(name)}
         >
-          All
+          {label}
         </button>
-        <button
-          type="button"
-          className="btn btn-outline-secondary"
-          onClick={this.onActiveClick}
-        >
-          Active
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-secondary"
-          onClick={this.onDoneClick}
-        >
-          Done
-        </button>
-      </div>
-    );
+      );
+    });
+
+    return <div className="btn-group">{buttons}</div>;
   }
 }
-// const ItemStatusFilterfunc = () => {
-//   return (
-//     <div className="btn-group">
-//       <button type="button" className="btn btn-info">
-//         All
-//       </button>
-//       <button type="button" className="btn btn-outline-secondary">
-//         Active
-//       </button>
-//       <button type="button" className="btn btn-outline-secondary">
-//         Done
-//       </button>
-//     </div>
-//   );
-// };
-
-///
